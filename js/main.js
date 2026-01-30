cf4c4658c6f816d7c4f9a014fe3fd807e70347dc
Pavan kalyan Aningi <116421166+pavananingi@users.noreply.github.com>
2026-01-30 10:40:22 +0530

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,8 @@
+window.addEventListener('scroll', () => {
+  document.querySelectorAll('.reveal').forEach(el => {
+    const top = el.getBoundingClientRect().top;
+    if (top < window.innerHeight - 100) {
+      el.classList.add('active');
+    }
+  });
+});
